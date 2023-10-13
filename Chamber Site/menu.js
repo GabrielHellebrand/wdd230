72382b5607406cf972e30b608d501e9e8d0630bf
Gabriel Hellebrand <hellebrandg@gmail.com>
2023-10-13 08:40:52 -0600

    added dark mode

--- Chamber Site/menu.js
+++ Chamber Site/menu.js
@@ -2,3 +2,15 @@ function toggleMenu() {
     var menu = document.getElementById("main-menu");
     menu.style.display = (menu.style.display === "block") ? "none" : "block";
 }
+
+function toggleDarkMode() {
+    var body = document.body;
+    body.classList.toggle("dark-mode");
+}
+document.addEventListener("DOMContentLoaded", function() {
+    var darkModeToggle = document.getElementById("dark-mode-checkbox");
+
+    darkModeToggle.addEventListener("change", function() {
+        toggleDarkMode();
+    });
+});
