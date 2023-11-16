e81aad81cf7f168fa47b7349aeec3bdbacb7695b
Gabriel Hellebrand <hellebrandg@gmail.com>
2023-11-16 15:51:10 -0700

    weather forecast

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,5 +1,7 @@
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=43.830043&lon=-111.829468&appid=a210d786daa3fb047488bb9d19cf6fb5&units=imperial"
 
+const forecasturl= "https://api.openweathermap.org/data/2.5/forecast?lat=43.830043&lon=-111.829468&appid=a210d786daa3fb047488bb9d19cf6fb5&units=imperial"
+
 function displayWeather(weatherData) {
   const desc = weatherData.weather[0].description;
   const windSpeed = weatherData.wind.speed.toFixed(0);
