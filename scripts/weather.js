45dc6fd7c919cc9ba4a377ae9901b12bdc42766a
Gabriel Hellebrand <hellebrandg@gmail.com>
2023-11-16 14:05:37 -0700

    Got weather sort of working

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,6 +1,4 @@
-const LON = "-117.4212318";
-const APIKEY = "a210d786daa3fb047488bb9d19cf6fb5";
-const url = "https://api.openweathermap.org/data/2.5/weather?lat={43.830043}&lon={-111.829468}&appid={a210d786daa3fb047488bb9d19cf6fb5}&units=imperial"
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=43.830043&lon=-111.829468&appid=a210d786daa3fb047488bb9d19cf6fb5&units=imperial"
 
 function displayWeather(weatherData) {
   const desc = weatherData.weather[0].description;
