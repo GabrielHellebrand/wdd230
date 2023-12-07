ad72463d841bc20fec231803839910cdcde49f34
Gabriel Hellebrand <hellebrandg@gmail.com>
2023-12-07 12:54:33 -0700

    Rebuilt everything from scratch, css looks better.

--- bountifulfoods/scripts/common.js
+++ bountifulfoods/scripts/common.js
@@ -1,1 +1,13 @@
 // hamburger menu stuff
+let menu = document.querySelector('.menuicon')
+let navbar = document.querySelector('.navbar')
+
+menu.onclick = () => {
+    menu.classList.toggle("move")
+    navbar.classList.toggle('openmenu')
+}
+// close menu while scrolling
+window.onscroll = () => {
+    menu.classList.remove("move")
+    navbar.classList.remove('openmenu')
+}
