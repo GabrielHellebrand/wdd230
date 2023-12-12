938f28fa3ccc315c98dbfe151e77aba110e1cec7
Gabriel Hellebrand <hellebrandg@gmail.com>
2023-12-12 11:27:28 -0700

    The weather and fruit still aren't displaying

--- bountifulfoods/scripts/weather.js
+++ bountifulfoods/scripts/weather.js
@@ -18,22 +18,22 @@ async function getWeatherData() {
       <p>Condition: ${data.weather[0].description}</p>
       <p>Humidity: ${data.main.humidity}%</p>`;
 
-    // Set up the weather icon  
+    // This sets up the weather icon  
     const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
     const desc = data.weather[0].description;
     let weatherIcon = document.getElementById("weather-icon");
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
 
-    // Fetch and display forecast for 9:00 AM for the next three days
+    // This fetches and displays the forecast for 9:00 AM for the next three days
     const forecastResponse = await fetch(forecastUrl);
     const forecastData = await forecastResponse.json();
     const forecastDetails = document.getElementById('forecast-details');
 
-    forecastDetails.innerHTML = ''; // Clear previous forecast data
+    forecastDetails.innerHTML = ''; // This should clear the previous forecast data
 
     for (let i = 0; i < 24; i += 8) {
-      // Convert Kelvin to Fahrenheit for forecast data
+      // This converts the Kelvin to Fahrenheit for forecast data
       const forecastTempFahrenheit = (forecastData.list[i].main.temp - 273.15) * 9/5 + 32;
 
       forecastDetails.innerHTML += `
